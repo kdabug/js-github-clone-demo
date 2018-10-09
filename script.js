43a7b3d0b9bd26ce4b6b2c8d8a923e3bdb45b140
Axylos <robertdraketalley@gmail.com>
2018-10-09 07:00:55 -0400

    add incomplete functions

---
+++ script.js
@@ -0,0 +1,11 @@
+// sum takes two Numbers as arguments
+// and should return their sum
+function sum(a, b) {
+}
+
+// shout takes a string as an argument
+// and it should return the string in all caps
+// Hint: check out the toUpperCase() method
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+function shout(message) {
+}
